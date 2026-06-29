f9524ac843f11045a05fed07c04382b7dd9d0e00
XR_XharpRazor <114336132+XR-XharpRazor@users.noreply.github.com>
2026-06-29 18:25:22 +0800

    added checkboxes to toggle different parts

--- main.js
+++ main.js
@@ -68,6 +68,116 @@ function POwO_Math_IsInRange_Exclusive(inMin, inVal, inMax)
     return inMin < inVal && inVal < inMax
 }
 
+
+
+
+// ---- ---- ---- ---- SETUP
+
+const canvas = document.getElementById("kanvas");
+const ctx = canvas.getContext("2d");
+const HTML_Body = POwO_docgetel("HTML_body")
+
+var field_checkBox_showR_Line = POwO_docgetel("field_checkBox_showR_Line");
+var field_checkBox_showX_Line = POwO_docgetel("field_checkBox_showX_Line");
+var field_checkBox_showY_Line = POwO_docgetel("field_checkBox_showY_Line");
+
+var field_checkBox_showR_Tag = POwO_docgetel("field_checkBox_showR_Tag");
+var field_checkBox_showX_Tag = POwO_docgetel("field_checkBox_showX_Tag");
+var field_checkBox_showY_Tag = POwO_docgetel("field_checkBox_showY_Tag");
+
+var field_checkBox_showAngle_Total = POwO_docgetel("field_checkBox_showAngle_Total")
+var field_checkBox_showAngle_Acute = POwO_docgetel("field_checkBox_showAngle_Acute")
+
+
+var GLOBAL_RingRadius = 320
+var GLOBAL_TickMarkSize = 20
+var GLOBAL_Angle = 60/360 * 2 * Math.PI
+var GLOBAL_HandleRadius = 16
+var GLOBAL_Handle2Radius = 8
+var GLOBAL_TickMarkTextOffset = 30
+
+var GLOBAL_CenterX = canvas.width / 2
+var GLOBAL_CenterY = canvas.height / 2
+var GLOBAL_isHold = false
+var GLOBAL_isSnap = false
+
+
+class ShOwOpe
+{
+    constructor(inPosX, inPosY, inPosZ, inType, inParam0, inParam1, inColorStroke, inStrokeWidth, inColorFill)
+    {
+        this.PosX = inPosX
+        this.PosY = inPosY
+        this.PosZ = inPosZ
+        this.Type = inType
+        this.Param0 = inParam0
+        this.Param1 = inParam1
+        this.ColorStroke = inColorStroke
+        this.StrokeWidth = inStrokeWidth
+        this.ColorFill = inColorFill
+    }
+
+    drawMe(inKanvasContext)
+    {
+        inKanvasContext.beginPath()
+
+        switch (this.Type)
+        {
+
+            case "circle":
+                inKanvasContext.arc(this.PosX, this.PosY, this.Param0, 0, Math.PI * 2);
+            break;
+
+            case "rect" :
+                let temp_RadiusX = this.Param0 / 2
+                let temp_RadiusY = this.Param1 / 2
+                inKanvasContext.rect(this.PosX - temp_RadiusX , this.PosY - temp_RadiusY , this.Param0, this.Param1);
+            break;
+        
+            default:
+            break;
+        }
+
+        inKanvasContext.fillStyle = this.ColorFill;
+        inKanvasContext.strokeStyle = this.ColorStroke;
+        inKanvasContext.lineWidth = this.StrokeWidth;
+
+        inKanvasContext.fill();
+        inKanvasContext.stroke();
+    }
+
+    isInside(mouseX, mouseY)
+    {
+        switch(this.Type)
+        {
+            case "circle":
+                let dx = mouseX - this.PosX;
+                let dy = mouseY - this.PosY;
+                return (dx*dx + dy*dy) <= (this.Param0*this.Param0);
+            break;
+
+            case "rect":
+                let temp_RadiusX = this.Param0 / 2
+                let temp_RadiusY = this.Param1 / 2
+
+                return (
+                    mouseX >= this.PosX - temp_RadiusX &&
+                    mouseX <= this.PosX + temp_RadiusX &&
+                    mouseY >= this.PosY - temp_RadiusY &&
+                    mouseY <= this.PosY + temp_RadiusY
+                );
+            break;
+
+            default: return false; break;
+        }
+
+        return false;
+    }
+}
+
+
+
+
 // ---- ---- ---- ---- page function
 
 function POwO_getMouse(event)
@@ -176,74 +286,91 @@ function POwO_RedrawAll()
     let temp_Acute_start = 0
     let temp_Acute_end = 0
     let temp_Angle_Moded = GLOBAL_Angle % (Math.PI * 2)
-    ctx.beginPath();
-    ctx.moveTo(GLOBAL_CenterX , GLOBAL_CenterY)
-    ctx.arc( GLOBAL_CenterX , GLOBAL_CenterY , 100 , 0 , - temp_Angle_Moded ,true);
-    ctx.lineTo(GLOBAL_CenterX, GLOBAL_CenterY)
-    ctx.fillStyle = "rgba(255,255,255,0.25)"
-    ctx.fill()
-    if (0 < temp_Angle_Moded && temp_Angle_Moded < Math.PI * 2 * 0.25) //if we use atan2 to detect angels, the range is -180 to 180
+    if (field_checkBox_showAngle_Total.checked)
     {
-        //1st quad
-        temp_Acute_start = 0
-        temp_Acute_end = temp_Angle_Moded
+        ctx.beginPath();
+        ctx.moveTo(GLOBAL_CenterX , GLOBAL_CenterY)
+        ctx.arc( GLOBAL_CenterX , GLOBAL_CenterY , 100 , 0 , - temp_Angle_Moded ,true);
+        ctx.lineTo(GLOBAL_CenterX, GLOBAL_CenterY)
+        ctx.fillStyle = "rgba(255,255,255,0.25)"
+        ctx.fill()
     }
-    else if (Math.PI * 2 * 0.25 < temp_Angle_Moded && temp_Angle_Moded < Math.PI * 2 * 0.5)
+    if (field_checkBox_showAngle_Acute.checked)
     {
-        //2nd quad
-        temp_Acute_start = temp_Angle_Moded
-        temp_Acute_end = Math.PI
+        if (0 < temp_Angle_Moded && temp_Angle_Moded < Math.PI * 2 * 0.25) //if we use atan2 to detect angels, the range is -180 to 180
+        {
+            //1st quad
+            temp_Acute_start = 0
+            temp_Acute_end = temp_Angle_Moded
+        }
+        else if (Math.PI * 2 * 0.25 < temp_Angle_Moded && temp_Angle_Moded < Math.PI * 2 * 0.5)
+        {
+            //2nd quad
+            temp_Acute_start = temp_Angle_Moded
+            temp_Acute_end = Math.PI
+        }
+        else if (- Math.PI * 2 * 0.5 < temp_Angle_Moded && temp_Angle_Moded < - Math.PI * 2 * 0.25)
+        {
+            //3rd quad
+            temp_Acute_start = Math.PI
+            temp_Acute_end = temp_Angle_Moded
+        }
+        else if (- Math.PI * 2 * 0.25 < temp_Angle_Moded && temp_Angle_Moded < 0)
+        {
+            //4th quad
+            temp_Acute_start = temp_Angle_Moded
+            temp_Acute_end = 0
+        }
+        else
+        {
+            temp_Acute_start = 0
+            temp_Acute_end = 0
+        }
+        ctx.beginPath();
+        ctx.moveTo(GLOBAL_CenterX , GLOBAL_CenterY)
+        ctx.arc( GLOBAL_CenterX , GLOBAL_CenterY , 50 , - temp_Acute_start , - temp_Acute_end ,true);
+        ctx.lineTo(GLOBAL_CenterX, GLOBAL_CenterY)
+        ctx.fillStyle = "rgba(255,192,0,1)"
+        ctx.fill()
     }
-    else if (- Math.PI * 2 * 0.5 < temp_Angle_Moded && temp_Angle_Moded < - Math.PI * 2 * 0.25)
-    {
-        //3rd quad
-        temp_Acute_start = Math.PI
-        temp_Acute_end = temp_Angle_Moded
-    }
-    else if (- Math.PI * 2 * 0.25 < temp_Angle_Moded && temp_Angle_Moded < 0)
-    {
-        //4th quad
-        temp_Acute_start = temp_Angle_Moded
-        temp_Acute_end = 0
-    }
-    else
-    {
-        temp_Acute_start = 0
-        temp_Acute_end = 0
-    }
-    ctx.beginPath();
-    ctx.moveTo(GLOBAL_CenterX , GLOBAL_CenterY)
-    ctx.arc( GLOBAL_CenterX , GLOBAL_CenterY , 50 , - temp_Acute_start , - temp_Acute_end ,true);
-    ctx.lineTo(GLOBAL_CenterX, GLOBAL_CenterY)
-    ctx.fillStyle = "rgba(255,192,0,1)"
-    ctx.fill()
+    
+    
 
     
 
     //draw radius
-    ctx.beginPath();
-    ctx.moveTo( GLOBAL_CenterX , GLOBAL_CenterY )
-    ctx.lineTo( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY - temp_deltaY );
-    ctx.lineWidth = 5
-    ctx.strokeStyle = "#FFC000"
-    ctx.stroke()
-
+    if (field_checkBox_showR_Line.checked)
+    {
+        ctx.beginPath();
+        ctx.moveTo( GLOBAL_CenterX , GLOBAL_CenterY )
+        ctx.lineTo( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY - temp_deltaY );
+        ctx.lineWidth = 5
+        ctx.strokeStyle = "#FFC000"
+        ctx.stroke()
+    }
+    
     //draw deltaX
-    ctx.beginPath();
-    ctx.moveTo( GLOBAL_CenterX , GLOBAL_CenterY )
-    ctx.lineTo( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY);
-    ctx.lineWidth = 5
-    ctx.strokeStyle = "#FF0000"
-    ctx.stroke()
-
+    if (field_checkBox_showX_Line.checked)
+    {
+        ctx.beginPath();
+        ctx.moveTo( GLOBAL_CenterX , GLOBAL_CenterY )
+        ctx.lineTo( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY);
+        ctx.lineWidth = 5
+        ctx.strokeStyle = "#FF0000"
+        ctx.stroke()
+    }
+    
     //draw deltaY
-    ctx.beginPath();
-    ctx.moveTo( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY )
-    ctx.lineTo( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY - temp_deltaY );
-    ctx.lineWidth = 5
-    ctx.strokeStyle = "#00C040"
-    ctx.stroke()
-
+    if (field_checkBox_showY_Line.checked)
+    {
+        ctx.beginPath();
+        ctx.moveTo( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY )
+        ctx.lineTo( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY - temp_deltaY );
+        ctx.lineWidth = 5
+        ctx.strokeStyle = "#00C040"
+        ctx.stroke()
+    }
+    
     //draw handle
     ctx.beginPath();
     ctx.arc( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY - temp_deltaY , GLOBAL_HandleRadius, 0, Math.PI * 2);
@@ -254,124 +381,39 @@ function POwO_RedrawAll()
     ctx.stroke();
     
     //draw secondary handle
-    ctx.beginPath();
-    ctx.arc( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY , GLOBAL_Handle2Radius, 0, Math.PI * 2);
-    ctx.lineWidth = 0;
-    ctx.fillStyle = '#FFFFFF';
-    ctx.fill();
-
+    if (field_checkBox_showX_Line.checked || field_checkBox_showY_Line.checked)
+    {
+        ctx.beginPath();
+        ctx.arc( GLOBAL_CenterX + temp_deltaX , GLOBAL_CenterY , GLOBAL_Handle2Radius, 0, Math.PI * 2);
+        ctx.lineWidth = 0;
+        ctx.fillStyle = '#FFFFFF';
+        ctx.fill();
+    }
+    
     //draw origin point
-    ctx.beginPath();
-    ctx.arc( GLOBAL_CenterX, GLOBAL_CenterY , 8, 0, Math.PI * 2);
-    ctx.lineWidth = 24;
-    ctx.fillStyle = '#FFFFFF';
-    ctx.strokeStyle = "rgba(255,255,255,0.25)"
-    ctx.fill();
-    ctx.stroke()
-
-
+    if (field_checkBox_showR_Line)
+    {
+        ctx.beginPath();
+        ctx.arc( GLOBAL_CenterX, GLOBAL_CenterY , 8, 0, Math.PI * 2);
+        ctx.lineWidth = 24;
+        ctx.fillStyle = '#FFFFFF';
+        ctx.strokeStyle = "rgba(255,255,255,0.25)"
+        ctx.fill();
+        ctx.stroke()
+    }
+    
     //draw text to show line labels
     let temp_TagCoord_X = ( GLOBAL_CenterX * 2 + temp_deltaX ) / 2
     let temp_TagCoord_Y = (GLOBAL_CenterY * 2 - temp_deltaY) / 2
-    POwO_Kanvas_DrawTag(temp_TagCoord_X, temp_TagCoord_Y, 80, 50, 16, "#302010","R = 1","#FFC000","20px Calibri")
-    POwO_Kanvas_DrawTag(temp_TagCoord_X, GLOBAL_CenterY, 150, 50, 16, "#201010","Rcosθ = " + Math.cos(GLOBAL_Angle).toFixed(3),"#FF0000","20px Calibri")
-    POwO_Kanvas_DrawTag(GLOBAL_CenterX + temp_deltaX, temp_TagCoord_Y, 150,50,16, "#102010", "Rsinθ = " + Math.sin(GLOBAL_Angle).toFixed(3),"#00C040", "20px Calibri" )
     
+    if (field_checkBox_showR_Tag.checked){POwO_Kanvas_DrawTag(temp_TagCoord_X, temp_TagCoord_Y, 80, 50, 16, "#302010","R = 1","#FFC000","20px Calibri")}
+    if (field_checkBox_showX_Tag.checked){POwO_Kanvas_DrawTag(temp_TagCoord_X, GLOBAL_CenterY, 150, 50, 16, "#201010","Rcosθ = " + Math.cos(GLOBAL_Angle).toFixed(3),"#FF0000","20px Calibri")}
+    if (field_checkBox_showY_Tag.checked){POwO_Kanvas_DrawTag(GLOBAL_CenterX + temp_deltaX, temp_TagCoord_Y, 150,50,16, "#102010", "Rsinθ = " + Math.sin(GLOBAL_Angle).toFixed(3),"#00C040", "20px Calibri" )}
 }
 
 
 
-// ---- ---- ---- ---- SETUP
 
-const canvas = document.getElementById("kanvas");
-const ctx = canvas.getContext("2d");
-const HTML_Body = POwO_docgetel("HTML_body")
-
-var GLOBAL_RingRadius = 400
-var GLOBAL_TickMarkSize = 20
-var GLOBAL_Angle = 60/360 * 2 * Math.PI
-var GLOBAL_HandleRadius = 16
-var GLOBAL_Handle2Radius = 8
-var GLOBAL_TickMarkTextOffset = 30
-
-var GLOBAL_CenterX = canvas.width / 2
-var GLOBAL_CenterY = canvas.height / 2
-var GLOBAL_isHold = false
-var GLOBAL_isSnap = false
-
-
-class ShOwOpe
-{
-    constructor(inPosX, inPosY, inPosZ, inType, inParam0, inParam1, inColorStroke, inStrokeWidth, inColorFill)
-    {
-        this.PosX = inPosX
-        this.PosY = inPosY
-        this.PosZ = inPosZ
-        this.Type = inType
-        this.Param0 = inParam0
-        this.Param1 = inParam1
-        this.ColorStroke = inColorStroke
-        this.StrokeWidth = inStrokeWidth
-        this.ColorFill = inColorFill
-    }
-
-    drawMe(inKanvasContext)
-    {
-        inKanvasContext.beginPath()
-
-        switch (this.Type)
-        {
-
-            case "circle":
-                inKanvasContext.arc(this.PosX, this.PosY, this.Param0, 0, Math.PI * 2);
-            break;
-
-            case "rect" :
-                let temp_RadiusX = this.Param0 / 2
-                let temp_RadiusY = this.Param1 / 2
-                inKanvasContext.rect(this.PosX - temp_RadiusX , this.PosY - temp_RadiusY , this.Param0, this.Param1);
-            break;
-        
-            default:
-            break;
-        }
-
-        inKanvasContext.fillStyle = this.ColorFill;
-        inKanvasContext.strokeStyle = this.ColorStroke;
-        inKanvasContext.lineWidth = this.StrokeWidth;
-
-        inKanvasContext.fill();
-        inKanvasContext.stroke();
-    }
-
-    isInside(mouseX, mouseY)
-    {
-        switch(this.Type)
-        {
-            case "circle":
-                let dx = mouseX - this.PosX;
-                let dy = mouseY - this.PosY;
-                return (dx*dx + dy*dy) <= (this.Param0*this.Param0);
-            break;
-
-            case "rect":
-                let temp_RadiusX = this.Param0 / 2
-                let temp_RadiusY = this.Param1 / 2
-
-                return (
-                    mouseX >= this.PosX - temp_RadiusX &&
-                    mouseX <= this.PosX + temp_RadiusX &&
-                    mouseY >= this.PosY - temp_RadiusY &&
-                    mouseY <= this.PosY + temp_RadiusY
-                );
-            break;
-
-            default: return false; break;
-        }
-
-        return false;
-    }
-}
 
 canvas.addEventListener("mousedown", (event) => {
 
